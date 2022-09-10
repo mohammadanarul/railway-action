589e2260ee0bf2397da95668cd835d7e49aeaad0
mohammadanarul <mdanarul7075@gmail.com>
2022-09-11 02:57:24 +0600

    some file updated.

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, AboutView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('about/', AboutView.as_view(), name='about'),
 ]
